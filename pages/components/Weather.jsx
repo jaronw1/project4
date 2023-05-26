13e5d0aed56ceabfcd3f1a4f16b9fcc116c83722
jaronw1 <jaronwong6@gmail.com>
2023-05-26 08:48:09 -0700

    deplloy

--- pages/components/Weather.jsx
+++ pages/components/Weather.jsx
@@ -6,18 +6,24 @@ const Weather = ({ data, forecast }) => {
     ? forecast.list.filter((item, index) => index % 8 === 0)
     : []
 
+  if (!data || !data.weather || !data.main) {
+    return <div>Loading...</div>
+  }
+
   return (
     <div className='relative flex flex-col justify-between max-w-[500px] w-full h-[90vh] m-auto p-4 text-white z-10'>
       {/* Top */}
       <div className='relative flex justify-between pt-12'>
         <div className='flex flex-col items-center'>
-          <Image
-            src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
-            alt='Weather Icon'
-            width={100}
-            height={100}
-          />
-          <p className='text-2xl'>{data.weather[0].main}</p>
+          {data.weather[0]?.icon && (
+            <Image
+              src={`http://openweathermap.org/img/wn/${data.weather[0].icon}.png`}
+              alt='Weather Icon'
+              width={100}
+              height={100}
+            />
+          )}
+          <p className='text-2xl'>{data.weather[0]?.main}</p>
         </div>
         <p className='text-8xl'>{data.main.temp.toFixed(0)}&#176;</p>
       </div>
@@ -39,7 +45,7 @@ const Weather = ({ data, forecast }) => {
             {/* Render temperature for each day */}
             <p className='font-bold'>{formatDate(item.dt)}</p>
             <p>{convertKelvinToFahrenheit(item.main.temp)}°</p>
-            {item.weather && item.weather.length > 0 && (
+            {item.weather && item.weather.length > 0 && item.weather[0]?.icon && (
               <Image
                 src={`http://openweathermap.org/img/wn/${item.weather[0].icon}.png`}
                 alt='Weather Icon'
@@ -62,14 +68,14 @@ const WeatherInfo = ({ label, value }) => (
 )
 
 const convertKelvinToFahrenheit = (kelvin) => {
-  const fahrenheit = (kelvin - 273.15) * (9 / 5) + 32;
-  return fahrenheit.toFixed(0);
+  const fahrenheit = (kelvin - 273.15) * (9 / 5) + 32
+  return fahrenheit.toFixed(0)
 }
 
 const formatDate = (timestamp) => {
-  const date = new Date(timestamp * 1000);
-  const options = { month: 'short', day: 'numeric' };
-  return date.toLocaleDateString(undefined, options);
+  const date = new Date(timestamp * 1000)
+  const options = { month: 'short', day: 'numeric' }
+  return date.toLocaleDateString(undefined, options)
 }
 
-export default Weather;
+export default Weather
